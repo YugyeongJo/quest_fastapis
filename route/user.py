2d8714bfa188811770803780972288cfba90b31c
scvgong <gongmyeongyun@gmail.com>
2024-01-08 14:28:45 +0900

    add code

--- route/user.py
+++ route/user.py
@@ -9,19 +9,19 @@ templates = Jinja2Templates(directory = "templates/")
 # problems.html 연결
 @router.get("/problems")
 async def problems(request:Request):
-    return templates.TemplateResponse(name="/users/problems.html",context={'request':request})
+    return templates.TemplateResponse(name="problems.html",context={'request':request})
 
 # list.html 연결
 @router.post("/lists")
 async def lists(request:Request):
-    return templates.TemplateResponse(name="/users/lists.html",context={'request':request})
+    return templates.TemplateResponse(name="lists.html",context={'request':request})
 
 # reads.html 연결
 @router.post("/reads")
 async def reads(request:Request):
-    return templates.TemplateResponse(name="users/reads.html",context={'request':request})
+    return templates.TemplateResponse(name="reads.html",context={'request':request})
 
 # main.html 연결
-@router.get("/main")
+@router.post("/main")
 async def main(request:Request):
-    return templates.TemplateResponse(name="users/main.html",context={'request':request})
+    return templates.TemplateResponse(name="main.html",context={'request':request})
